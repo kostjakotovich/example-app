482227d04d40a2106dd51331d7a0b6af359f2226
unknown <kostja.kotovich@gmail.com>
2023-05-09 17:32:36 +0300

    Addes registration form

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="border: 3px solid black;">
+    <h2>Registration</h2>
+    <form action="">
+        <input type="text" placeholder="Name">
+        <input type="text" placeholder="Email">
+        <input type="password" placeholder="Password">
+        <button>Register</button>
+    </form>
+</body>
+</html>
